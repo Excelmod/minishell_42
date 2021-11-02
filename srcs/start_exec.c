275d209194c8917ade0a471839c59389f0d10c63
Adupav <adupav2000@gmail.com>
2021-11-02 16:37:17 +0100

    no modiff

--- srcs/start_exec.c
+++ srcs/start_exec.c
@@ -3,129 +3,129 @@
 /*                                                        :::      ::::::::   */
 /*   start_exec.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ljulien <ljulien@student.42.fr>            +#+  +:+       +#+        */
+/*   By: adu-pavi <adu-pavi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/30 17:06:37 by lchristo          #+#    #+#             */
-/*   Updated: 2021/10/29 23:56:34 by ljulien          ###   ########.fr       */
+/*   Updated: 2021/11/01 10:23:04 by adu-pavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int     check_builtin(t_shell *shell, char *str)
+int	check_builtin(t_shell *shell, char *str)
 {
-    int i;
+	int	i;
 
-    i = -1;
-    if (compare(str, "echo"))
-        i = builtin_echo(shell->cmd->cmds);
-    if (compare(str, "env"))
-        i = builtin_env(shell);
-    if (compare(str, "export"))
-        i = builtin_export(shell, shell->cmd->cmds);
-    if (compare(str, "unset"))
-        i = builtin_unset(shell, shell->cmd->cmds);
-    if (compare(str, "pwd"))
-        i = builtin_pwd(shell);
+	i = -1;
+	if (compare(str, "echo"))
+		i = builtin_echo(shell->cmd->cmds);
+	if (compare(str, "env"))
+		i = builtin_env(shell);
+	if (compare(str, "export"))
+		i = builtin_export(shell, shell->cmd->cmds);
+	if (compare(str, "unset"))
+		i = builtin_unset(shell, shell->cmd->cmds);
+	if (compare(str, "pwd"))
+		i = builtin_pwd(shell);
 	if (compare(str, "cd"))
 		i = builtin_cd(shell, shell->cmd->cmds);
-    if (i > 0)
-        printf("problem\n");
-    return (i);
+	if (i > 0)
+		printf("problem\n");
+	return (i);
 }
 
-int     path(t_shell *shell)
+int path(t_shell *shell)
 {
-    int i;
-    char *str;
+	int		i;
+	char	*str;
 
-    i = 0;
-    str = ft_strjoin("/", shell->cmd->cmds[0]);
-    // if (shell->cmd->fd_out != -1)
-        // dup2(shell->cmd->fd_out, shell->stdout);
-    if (check_builtin(shell, shell->cmd->cmds[0]) != -1)
-        exit (1) ;
-    while (shell->path[i] != NULL)
-    {
-        execve(ft_strjoin(shell->path[i], str), shell->cmd->cmds, shell->env);
-        i++;
-    }
-    execve(ft_strjoin("./", str), shell->cmd->cmds, shell->env);
-    printf("minishell: %s: command not found\n", shell->cmd->cmds[0]);
-    return (1);
+	i = 0;
+	str = ft_strjoin("/", shell->cmd->cmds[0]);
+	// if (shell->cmd->fd_out != -1)
+	// dup2(shell->cmd->fd_out, shell->stdout);
+	if (check_builtin(shell, shell->cmd->cmds[0]) != -1)
+		exit(1);
+	while (shell->path[i] != NULL)
+	{
+		execve(ft_strjoin(shell->path[i], str), shell->cmd->cmds, shell->env);
+		i++;
+	}
+	execve(ft_strjoin("./", str), shell->cmd->cmds, shell->env);
+	printf("minishell: %s: command not found\n", shell->cmd->cmds[0]);
+	return (1);
 }
 
-void    print_error(t_shell *shell)
+void print_error(t_shell *shell)
 {
-    ft_putendl_fd(shell->cmd->msg_error, 2);
-    free(shell->cmd->msg_error);
+	ft_putendl_fd(shell->cmd->msg_error, 2);
+	free(shell->cmd->msg_error);
 }
 
-t_cmd   *first_cmd(t_shell *shell)
+t_cmd *first_cmd(t_shell *shell)
 {
-    t_cmd *next;
+	t_cmd *next;
 
-    next = shell->cmd;
-    if (shell->cmd->prev == NULL)
-        return (shell->cmd);
-    while (next->prev != NULL)
-        next = next->prev;
-    return (next);
+	next = shell->cmd;
+	if (shell->cmd->prev == NULL)
+		return (shell->cmd);
+	while (next->prev != NULL)
+		next = next->prev;
+	return (next);
 }
 
-int     nb_pipes(t_shell *shell)
+int nb_pipes(t_shell *shell)
 {
-    t_cmd   *next;
-    int     i;
+	t_cmd *next;
+	int i;
 
-    i = 0;
-    next = shell->cmd;
-    while (next->next != NULL)
-    {
-        i++;
-        next = next->next;
-    }
-    return (i);
+	i = 0;
+	next = shell->cmd;
+	while (next->next != NULL)
+	{
+		i++;
+		next = next->next;
+	}
+	return (i);
 }
 
-void    ft_build_pipes(t_shell *shell, int i)
+void ft_build_pipes(t_shell *shell, int i)
 {
-    while(i > 0)
-    {
-        pipe(&shell->tpipe[i]);
-        i--;
-    }
+	while (i > 0)
+	{
+		pipe(&shell->tpipe[i]);
+		i--;
+	}
 }
 
-void    ft_build_tabs(t_shell *shell)
+void ft_build_tabs(t_shell *shell)
 {
-    int nb_pipe;
-    //int nb_pid;
+	int nb_pipe;
+	// int nb_pid;
 
-    nb_pipe = nb_pipes(shell);
-    shell->tpipe = malloc(sizeof(int) * nb_pipe * 2);
-    ft_build_pipes(shell, nb_pipe);
-    shell->tpid = malloc(sizeof(int) * nb_pipe + 1);
+	nb_pipe = nb_pipes(shell);
+	shell->tpipe = malloc(sizeof(int) * nb_pipe * 2);
+	ft_build_pipes(shell, nb_pipe);
+	shell->tpid = malloc(sizeof(int) * nb_pipe + 1);
 }
 
-void    starting_execution(t_shell *shell)
+void starting_execution(t_shell *shell)
 {
-    int i;
+	int i;
 
-    //ft_build_tabs(shell);
-    if (shell->cmd && shell->cmd->msg_error != NULL)
-    {
-        print_error(shell);
-        return ;
-    }
-    if (shell->cmd && shell->cmd->cmds && compare(shell->cmd->cmds[0], "exit"))
-        exit (0);
-    i = fork();
-    if (i == 0)
-    {
-        if (shell->cmd->cmds)
-            path(shell);
-    }
-    else
-        waitpid(i, NULL, 0);
+	// ft_build_tabs(shell);
+	if (shell->cmd && shell->cmd->msg_error != NULL)
+	{
+		print_error(shell);
+		return;
+	}
+	if (shell->cmd && shell->cmd->cmds && compare(shell->cmd->cmds[0], "exit"))
+		exit(0);
+	i = fork();
+	if (i == 0)
+	{
+		if (shell->cmd->cmds)
+			path(shell);
+	}
+	else
+		waitpid(i, NULL, 0);
 }
